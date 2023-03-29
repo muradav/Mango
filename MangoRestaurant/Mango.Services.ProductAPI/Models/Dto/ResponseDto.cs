a8e78b7ae136bce6b7b1fc3d6ab2f8501e2687a6
muradav <muradav@code.edu.az>
2023-03-29 16:53:44 +0400

    feat:  Product Repository Interface

--- MangoRestaurant/Mango.Services.ProductAPI/Models/Dto/ResponseDto.cs
+++ MangoRestaurant/Mango.Services.ProductAPI/Models/Dto/ResponseDto.cs
@@ -2,5 +2,9 @@
 {
     public class ResponseDto
     {
+        public bool IsSuccess { get; set; } = true;
+        public object Result { get; set; }
+        public string DisplayMessage { get; set; } = "";
+        public List<string> ErrorMessages { get; set; }
     }
 }
--- MangoRestaurant/Mango.Services.ProductAPI/Repository/IProductRepository.cs
+++ MangoRestaurant/Mango.Services.ProductAPI/Repository/IProductRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace Mango.Services.ProductAPI.Repository
+﻿using Mango.Services.ProductAPI.Models.Dtos;
+
+namespace Mango.Services.ProductAPI.Repository
 {
     public interface IProductRepository
     {
+        Task<IEnumerable<ProductDto>> GetProducts();
+        Task<ProductDto> GetProductById (int productId);
+        Task<ProductDto> CreateUpdateProduct(ProductDto product);
+        Task<bool> DeleteProduct(int productId);
     }
 }
