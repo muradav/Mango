422bad7104cff2c9f294d387d87a2604a887ea35
muradav <muradav@code.edu.az>
2023-03-31 12:52:06 +0400

    feat: ProductAPI Controller HTTPGET

---
+++ MangoRestaurant/Mango.Services.ProductAPI/Controllers/ProductApiController.cs
@@ -0,0 +1,57 @@
+﻿using Azure;
+using Mango.Services.ProductAPI.Models.Dto;
+using Mango.Services.ProductAPI.Models.Dtos;
+using Mango.Services.ProductAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ProductAPI.Controllers
+{
+    [Route("api/products")]
+    public class ProductApiController : ControllerBase
+    {
+        protected ResponseDto _response;
+        private IProductRepository _productRepository;
+
+        public ProductApiController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+
+            this._response = new ResponseDto();
+        }
+
+        [HttpGet]
+        public async Task<object> Get()
+        {
+            try
+            {
+                IEnumerable<ProductDto> productDtos = await _productRepository.GetProducts();
+                _response.Result = productDtos;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                    = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<object> Get(int id)
+        {
+            try
+            {
+                ProductDto productDto = await _productRepository.GetProductById(id);
+                _response.Result = productDto;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                    = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+    }
+}
