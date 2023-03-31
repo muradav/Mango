df993f101467c4874727f472864182e72029c271
muradav <muradav@code.edu.az>
2023-03-31 17:41:40 +0400

    feat: Azure Storage Explorer for Images. Migration Seed Product. Mapping fix

--- MangoRestaurant/Mango.Services.ProductAPI/MappingConfig.cs
+++ MangoRestaurant/Mango.Services.ProductAPI/MappingConfig.cs
@@ -11,6 +11,7 @@ namespace Mango.Services.ProductAPI
             var mappingConfig = new MapperConfiguration(config =>
             {
                 config.CreateMap<ProductDto, Product>();
+                config.CreateMap<Product, ProductDto>();
             });
 
             return mappingConfig;
