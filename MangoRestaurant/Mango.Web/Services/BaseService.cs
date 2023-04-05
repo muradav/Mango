26bf25713b562fbc69924a2918279ede47b1cbcb
muradav <muradav@code.edu.az>
2023-04-05 16:21:59 +0400

    feat: Base Service Implementation

--- MangoRestaurant/Mango.Web/Services/BaseService.cs
+++ MangoRestaurant/Mango.Web/Services/BaseService.cs
@@ -1,6 +1,7 @@
 ﻿using Mango.Web.Models;
 using Mango.Web.Services.IServices;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace Mango.Web.Services
 {
@@ -16,7 +17,7 @@ namespace Mango.Web.Services
         }
 
 
-        public Task<T> SendAsync<T>(ApiRequest apiRequest)
+        public async Task<T> SendAsync<T>(ApiRequest apiRequest)
         {
             try
             {
@@ -27,13 +28,46 @@ namespace Mango.Web.Services
                 client.DefaultRequestHeaders.Clear();
                 if (apiRequest.Data != null)
                 {
-                    message.Content = new StringContent(JsonConvert)
+                    message.Content = new StringContent(JsonConvert.SerializeObject(apiRequest.Data),
+                        Encoding.UTF8, "application/json");
                 }
+
+                HttpResponseMessage apiResponse = null;
+                switch (apiRequest.ApiType)
+                {
+                    case SD.ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case SD.ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    case SD.ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+                apiResponse = await client.SendAsync(message);
+
+                var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                var apiResponseDto = JsonConvert.DeserializeObject<T>(apiContent);
+
+                return apiResponseDto;
             }
             catch (Exception ex)
             {
+                var dto = new ResponseDto
+                {
+                    DisplayMessage = "Error",
+                    ErrorMessages = new List<string> { Convert.ToString(ex.Message) },
+                    IsSuccess = false
+                };
+                var res = JsonConvert.SerializeObject(dto);
+                var apiResponseDto = JsonConvert.DeserializeObject<T>(res);
 
-                throw;
+                return apiResponseDto;
+
             }
         }
 
