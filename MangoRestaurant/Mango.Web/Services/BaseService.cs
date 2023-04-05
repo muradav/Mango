0127b6624b5a44272dac457ec7fedfa09be629a5
muradav <muradav@code.edu.az>
2023-04-05 16:19:04 +0400

    feat: Base Service Implementation

--- MangoRestaurant/Mango.Web/Services/BaseService.cs
+++ MangoRestaurant/Mango.Web/Services/BaseService.cs
@@ -1,6 +1,45 @@
-﻿namespace Mango.Web.Services
+﻿using Mango.Web.Models;
+using Mango.Web.Services.IServices;
+using Newtonsoft.Json;
+
+namespace Mango.Web.Services
 {
-    public class BaseService
+    public class BaseService : IBaseService
     {
+        public ResponseDto responseModel { get; set; }
+        public IHttpClientFactory httpClient { get; set; }
+
+        public BaseService(IHttpClientFactory httpClient)
+        {
+            this.responseModel = new ResponseDto();
+            this.httpClient = httpClient;
+        }
+
+
+        public Task<T> SendAsync<T>(ApiRequest apiRequest)
+        {
+            try
+            {
+                var client = httpClient.CreateClient("MangoApi");
+                HttpRequestMessage message = new HttpRequestMessage();
+                message.Headers.Add("Accept", "application/json");
+                message.RequestUri= new Uri(apiRequest.Url);
+                client.DefaultRequestHeaders.Clear();
+                if (apiRequest.Data != null)
+                {
+                    message.Content = new StringContent(JsonConvert)
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(true);
+        }
     }
 }
